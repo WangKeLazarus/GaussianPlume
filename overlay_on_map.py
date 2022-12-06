3a7e66721f174ae4c3ab1ee3105151a978ca0518
Lazarus Wang <113286242+WangKeLazarus@users.noreply.github.com>
2022-12-06 15:42:53 +0100

    Initial commit

---
+++ overlay_on_map.py
@@ -0,0 +1,30 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from gaussianPlumeModel import x,y,C1
+
+def overlay_on_map():
+    import scipy.io as sio
+
+    # Overlay concentrations on map
+    plt.ion()
+    mat_contents = sio.loadmat('map') # We should upload our own map for the map_green_lane.mat file
+    plt.figure()
+    plt.imshow(mat_contents['A'], \
+       extent=[np.min(mat_contents['ximage']), \
+               np.max(mat_contents['ximage']), \
+               np.min(mat_contents['yimage']), \
+               np.max(mat_contents['yimage'])])
+               
+    plt.xlabel('x (m)') 
+    plt.ylabel('y (m)') 
+    cs=plt.contour(x,y,np.mean(C1,axis=2)*1e6, cmap='hot')
+    plt.clabel(cs,cs.levels,inline=True, fmt='%.1f',fontsize=5)
+
+    plt.show(block=False)
+    plt.pause(0)
+    plt.close()
+
+    return
+    
+if __name__ == "__main__":
+    overlay_on_map()
